ec8f804a57cfed14ea4d8c9fa744771a7eefb1f6
Frank Blau <fjblau@gmail.com>
2016-09-07 04:39:45 +0000

    Added loop code

--- iothash.py
+++ iothash.py
@@ -1,19 +1,25 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 
 import hashlib
 from crate import client
+import datetime
+import time
 
-connection = client.connect('http://192.168.99.100:32769')
+connection = client.connect('http://192.168.1.135:4200')
 cursor = connection.cursor()
-cursor.execute("select * from test order by testtime")
-recs = cursor.execute("select count(*) from test")
-cursor.execute("select * from test order by testtime")
-data = hashlib.sha256()
-result = "START"
-while result:
-	data.update(str(result))
-	result = cursor.fetchmany(2)
+
+while True:
+
+	tranmin = int(datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M')) -1 
+	print tranmin
+	cursor.execute("select * from berry where tranmin = ? order by sensordttm", (str(tranmin),))
+	data = hashlib.sha256()
+	result = "START"
+	while result:
+		result = cursor.fetchone()
+		data.update(str(result))
 	print(data.hexdigest())
+	time.sleep(60)
 	
 
 
